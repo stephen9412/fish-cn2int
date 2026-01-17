96467518451c0103075808fa52ccfc03ea9b7f44
Stephen J. Li <stephen94125@gmail.com>
2026-01-18 07:09:46 +0800

    refactor: streamline cn2int function and improve number parsing logic
    - Introduced a new helper function `_parse_number` to enhance the parsing of Chinese numerals, simplifying the extraction of thousands, hundreds, tens, and units.
    - Replaced repetitive string matching and splitting with a more efficient `_extract_and_consume` function to handle numeral markers.
    - Improved handling of edge cases for unit extraction based on input length, ensuring accurate conversions for various numeral formats.
    - These changes enhance the maintainability and performance of the `cn2int` function.

--- functions/cn2int.fish
+++ functions/cn2int.fish
@@ -9,72 +9,76 @@ function cn2int
 
     set input (_cn_map_digits $input)
 
+    set -l result (_parse_number $input)
+    echo $result[1]
+end
+
+
+function _parse_number
+    set -l input $argv[1]
+    
     set -l thousands 0
     set -l hundreds 0
     set -l tens 0
     set -l units 0
 
-    if string match -q '*千*' -- $input
-        set -l parts (string split "千" -- $input)
-        set thousands $parts[1]
-        if test -z "$thousands"
-            set thousands 1
-        end
-        set input $parts[2]
-    end
+    set thousands (_extract_and_consume $input '千')
+    set input $cn_extract_remainder
+    
+    set hundreds (_extract_and_consume $input '百')
+    set input $cn_extract_remainder
+    
+    set tens (_extract_and_consume $input '十')
+    set input $cn_extract_remainder
 
-    if string match -q '*百*' -- $input
-        set -l parts (string split "百" -- $input)
-        set hundreds $parts[1]
-        if test -z "$hundreds"
-            set hundreds 1
-        end
-        set input $parts[2]
-    end
-
-    if string match -q '*十*' -- $input
-        set -l parts (string split "十" -- $input)
-        set tens $parts[1]
-        if test -z "$tens"
-            set tens 1
-        end
-        set units $parts[2]
-        if test -z "$units"
-            set units 0
-        end
-    else
+    set -l len (string length -- $input)
+    if test $len -eq 0
+        set units 0
+    else if test $len -eq 1
         set units $input
-        if test -z "$units"
-            set units 0
+    else if test $len -eq 2
+        if test $thousands -eq 0 -a $hundreds -eq 0 -a $tens -eq 0
+            set tens (string sub -l 1 -- $input)
+            set units (string sub -s 2 -l 1 -- $input)
+        else
+            set tens (string sub -l 1 -- $input)
+            set units (string sub -s 2 -l 1 -- $input)
         end
-    end
-
-    set -l len (string length -- $units)
-    if test $len -eq 4
-        set thousands (string sub -l 1 -- $units)
-        set hundreds (string sub -s 2 -l 1 -- $units)
-        set tens (string sub -s 3 -l 1 -- $units)
-        set units (string sub -s 4 -l 1 -- $units)
     else if test $len -eq 3
         if test $thousands -eq 0
-            set hundreds (string sub -l 1 -- $units)
-            set tens (string sub -s 2 -l 1 -- $units)
-            set units (string sub -s 3 -l 1 -- $units)
+            set hundreds (string sub -l 1 -- $input)
+            set tens (string sub -s 2 -l 1 -- $input)
+            set units (string sub -s 3 -l 1 -- $input)
         else
-            set tens (string sub -s 2 -l 1 -- $units)
-            set units (string sub -s 3 -l 1 -- $units)
+            set tens (string sub -s 2 -l 1 -- $input)
+            set units (string sub -s 3 -l 1 -- $input)
         end
-    else if test $len -eq 2
-        if test $thousands -eq 0; and test $hundreds -eq 0
-            set tens (string sub -l 1 -- $units)
-            set units (string sub -s 2 -l 1 -- $units)
-        else
-            set tens (string sub -l 1 -- $units)
-            set units (string sub -s 2 -l 1 -- $units)
-        end
+    else if test $len -eq 4
+        set thousands (string sub -l 1 -- $input)
+        set hundreds (string sub -s 2 -l 1 -- $input)
+        set tens (string sub -s 3 -l 1 -- $input)
+        set units (string sub -s 4 -l 1 -- $input)
     end
 
     math "$thousands * 1000 + $hundreds * 100 + $tens * 10 + $units"
+end
+
+
+function _extract_and_consume
+    set -l input $argv[1]
+    set -l marker $argv[2]
+    
+    if string match -q "*$marker*" -- $input
+        set -l parts (string split $marker -- $input)
+        set -l value $parts[1]
+        test -z "$value"; and set value 1
+        set -g cn_extract_remainder $parts[2]
+        echo $value
+        return 0
+    end
+    set -g cn_extract_remainder $input
+    echo 0
+    return 1
 end
 
 
